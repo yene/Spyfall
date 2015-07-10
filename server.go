37f4ab487c5861c53fa18c782c6c5b3b0efbc89e
yene <yene@cocoadojo.com>
2015-07-10 21:21:14 +0200

    hashid checking

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 )
 import "github.com/speps/go-hashids"
 
+var currentGameID = 55
+
 // fmt.Fprintf(w, "GET, %q", html.EscapeString(r.URL.Path))
 // http.Error(w, "Invalid request method.", 405)
 
@@ -20,15 +22,25 @@ func main() {
 		hd.Salt = "super secret salt"
 		hd.MinLength = 4
 		h := hashids.NewWithData(hd)
-		e, _ := h.Encode([]int{45})
+		e, _ := h.Encode([]int{currentGameID})
 		fmt.Println(e)
-		d := h.Decode(e)
-		fmt.Println(d)
 
 	})
 
 	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
+			http.NotFound(w, r)
+			return
+		}
+		r.ParseForm()
+		lobbyID := r.Form["lobbyID"][0]
+		hd := hashids.NewData()
+		hd.Salt = "super secret salt"
+		h := hashids.NewWithData(hd)
+		d := h.Decode(lobbyID)
+		if d[0] == currentGameID {
+			fmt.Println(d)
+		} else {
 			http.NotFound(w, r)
 			return
 		}
