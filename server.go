2de0d92b93afa1aa885e564e19a7275ba75bf2ad
yene <yene@cocoadojo.com>
2015-07-10 21:09:06 +0200

    added hashid for parameter

--- server.go
+++ server.go
@@ -1,8 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+import "github.com/speps/go-hashids"
+
+// fmt.Fprintf(w, "GET, %q", html.EscapeString(r.URL.Path))
+// http.Error(w, "Invalid request method.", 405)
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.NotFound(w, r)
+			return
+		}
+		hd := hashids.NewData()
+		hd.Salt = "super secret salt"
+		hd.MinLength = 4
+		h := hashids.NewWithData(hd)
+		e, _ := h.Encode([]int{45})
+		fmt.Println(e)
+		d := h.Decode(e)
+		fmt.Println(d)
+
+	})
+
+	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.NotFound(w, r)
+			return
+		}
+
+	})
+
 	http.ListenAndServe(":3000", nil)
 }
