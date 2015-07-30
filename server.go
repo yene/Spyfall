8cf96274961c62201248b343c133769a11167970
yene <yene@cocoadojo.com>
2015-07-30 19:26:10 +0200

    parsing html templates

--- server.go
+++ server.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 import "github.com/speps/go-hashids"
 
-var currentGameID = 55
+var currentGameID = 55 //P0vQ
 
 // fmt.Fprintf(w, "GET, %q", html.EscapeString(r.URL.Path))
 // http.Error(w, "Invalid request method.", 405)
 
+type Page struct {
+	Code  string
+	Admin bool
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/room/new", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.NotFound(w, r)
 			return
@@ -22,12 +28,13 @@ func main() {
 		hd.Salt = "super secret salt"
 		hd.MinLength = 4
 		h := hashids.NewWithData(hd)
-		e, _ := h.Encode([]int{currentGameID})
-		fmt.Println(e)
-
+		c, _ := h.Encode([]int{currentGameID})
+		t, _ := template.ParseFiles("static/room.html")
+		p := &Page{Code: c, Admin: true}
+		t.Execute(w, p)
 	})
 
-	http.HandleFunc("/join", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/room/join", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			http.NotFound(w, r) // w.WriteHeader(http.StatusMethodNotAllowed) return
 			return
@@ -40,11 +47,13 @@ func main() {
 		d := h.Decode(lobbyID)
 		if d[0] == currentGameID {
 			fmt.Println(d)
+			t, _ := template.ParseFiles("static/room.html")
+			p := &Page{Code: lobbyID, Admin: false}
+			t.Execute(w, p)
 		} else {
 			http.NotFound(w, r)
 			return
 		}
-
 	})
 
 	http.ListenAndServe(":3000", nil)
